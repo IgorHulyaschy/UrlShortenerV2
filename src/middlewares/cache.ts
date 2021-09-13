b04592f13f4cb924233bd381e60ed2e4939d0c90
IgorHulyaschy <gulyaschy@gmail.com>
2021-09-13 10:38:53 +0300

    add errorcatcher

--- src/middlewares/cache.ts
+++ src/middlewares/cache.ts
@@ -2,20 +2,25 @@ import { AppContext, getAsync, setAsync } from '../app';
 import { cacheUrl } from '../url/types';
 
 export default (duration: number) => async (ctx: AppContext, next: () => Promise<void>) => {
-  if (ctx.method !== 'GET') {
-    console.error('Cannot cache non-GET methods!');
-    return next();
-  }
-  const key = ctx.originalUrl;
-  const longUrl = await getAsync(key);
-  if (longUrl !== null) {
-    console.log('redirect from cache');
-    ctx.redirect(longUrl);
-  } else {
-    await next();
-    console.log(`Cache miss for ${key}`);
-    const { responseUrl } = <cacheUrl>ctx.body;
-    await setAsync(key, duration, responseUrl.longUrl);
-    ctx.redirect(responseUrl.longUrl);
+  try {
+    if (ctx.method !== 'GET') {
+      console.error('Cannot cache non-GET methods!');
+      return next();
+    }
+    const key = ctx.originalUrl;
+    const longUrl = await getAsync(key);
+    if (longUrl !== null) {
+      console.log('redirect from cache');
+      ctx.redirect(longUrl);
+    } else {
+      await next();
+      console.log(`Cache miss for ${key}`);
+      const { responseUrl } = <cacheUrl>ctx.body;
+      await setAsync(key, duration, responseUrl.longUrl);
+      ctx.redirect(responseUrl.longUrl);
+    }
+  } catch (err) {
+    console.log(err);
+    ctx.status = 400;
   }
 };
